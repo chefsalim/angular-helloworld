fe09fdfb172689bfe678b591ee832e67ea741232
Salim Alam <salam@chef.io>
2016-09-14 12:17:42 -0700

    Boot with upgrade adapter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 /// <reference path="../typings/index.d.ts" />
-
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
 import * as angular from 'angular';
+import { UpgradeAdapter } from '@angular/upgrade';
 
 import {hello} from './app/hello';
 import 'angular-ui-router';
@@ -15,4 +17,6 @@ angular
   .config(routesConfig)
   .component('app', hello);
 
-angular.bootstrap(document.body, ['app'], {strictDi: false});
+console.log("Bootstrapping...");
+const upgradeAdapter = new UpgradeAdapter();
+upgradeAdapter.bootstrap(document.body, ['app'], {strictDi: false});
